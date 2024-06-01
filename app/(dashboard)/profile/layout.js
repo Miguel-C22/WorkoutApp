c24cb1000c5807e2f14f121889ff4bae24fc69c8
Miguel-C22 <110456521+Miguel-C22@users.noreply.github.com>
2024-06-01 00:48:03 -0500

    profile page update

--- app/(dashboard)/profile/layout.js
+++ app/(dashboard)/profile/layout.js
@@ -1,14 +1,53 @@
 "use client"
-import React from 'react'
-import ProfilePages from "../../../components/ProfilePages"
+import { useState } from 'react'
 import CreateWorkout  from "../../../components/CreateWorkout"
+import PostPage from "../../../components/Post-Private-Saved-Pages/PostPage"
+import Private from "../../../components/Post-Private-Saved-Pages/Private"
+import Saved from "../../../components/Post-Private-Saved-Pages/Saved"
 
 function layout({children}) {
+  const [selectedComponent, setSelectedComponent] = useState(null);
+
+  function handleComponentSelection(component) {
+    setSelectedComponent(component);
+    
+  }
+
+  function renderComponent() {
+    switch (selectedComponent) {
+        case 'profile':
+            return <PostPage />
+        case 'pr':
+            return  <Private />
+        case 'bio':
+            return <Saved />
+        default:
+            return null;
+    }
+}
+  
   return (
     <div>
-        {children}
-        <ProfilePages />
-        <CreateWorkout />
+      {children}
+      <CreateWorkout />
+      <div className="flex justify-center gap-4 mt-24">
+        <button 
+          onClick={() => handleComponentSelection('profile')}>
+          Posts
+        </button>
+        <span>|</span>
+        <button 
+          onClick={() => handleComponentSelection('pr')}>
+          Private
+        </button>
+        <span>|</span>
+        <button 
+          onClick={() => handleComponentSelection('bio')}>
+          Saved
+        </button>
+      </div>
+      {/* Render the selected component */}
+      {renderComponent()}
     </div>
   )
 }
--- app/(dashboard)/profile/page.js
+++ app/(dashboard)/profile/page.js
@@ -2,39 +2,51 @@ import React from "react"
 import UserProfileImage from "../../../components/UserProfileImage";
 import UserName from "../../../components/UserName";
 import useSaveNewUser from "../../../customHooks/saveNewUser"
+import {currentUser} from "@clerk/nextjs/server";
 
-function page() {
+async function page() {
+  const user = await currentUser()
 
+  let userData = []
   /* 
     This page is the initial landing page for users after signing in or opening the application.
     It runs a custom hook, useSaveNewUser, which is specifically designed to handle new user information 
     and save it to the server.
   */
   useSaveNewUser() 
-
+  
+   const response = await fetch(`http://localhost:3000/api/accountSettings/${user.id}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const data = await response.json();
+        userData = data.data[0]
   
   return (
-    <div className="flex items-center justify-center flex-wrap gap-4 px-2 md:flex-row lg:gap-44 md:p-2">
-        <UserProfileImage/>
-          <div className="flex flex-col gap-2 flex-wrap text-center md:text-left">
-            <UserName />
-          <div className="stats shadow flex flex-wrap sm:flex-nowrap">
-            <div className="stat shadow">
-              <div className="stat-title">Bench</div>
-              <div className="stat-value text-2xl">295 lbs</div>
-              <div className="stat-desc">Personal Record</div>
-            </div>           
-            <div className="stat shadow">
-            <div className="stat-title">Squat</div>
-              <div className="stat-value text-2xl">475 lbs</div>
-              <div className="stat-desc">Personal Record</div>
-            </div>         
-            <div className="stat shadow">
-            <div className="stat-title">Deadlift</div>
-              <div className="stat-value text-2xl">N/A lbs</div>
-              <div className="stat-desc">Personal Record</div>
-            </div>
+    <div className="flex items-center justify-center flex-col gap-4">
+      <UserProfileImage/>
+      <UserName />
+      <div className="stats shadow flex flex-wrap text-center mx-8 sm:flex-nowrap sm:text-left">
+        <div className="stat shadow">
+          <div className="stat-title">Bench</div>
+          <div className="stat-value text-2xl">{userData.prBench} lbs</div>
+          <div className="stat-desc">Personal Record</div>
+        </div>           
+        <div className="stat shadow">
+          <div className="stat-title">Squat</div>
+          <div className="stat-value text-2xl">{userData.prSquat} lbs</div>
+          <div className="stat-desc">Personal Record</div>
+          </div>         
+        <div className="stat shadow">
+          <div className="stat-title">Deadlift</div>
+          <div className="stat-value text-2xl">{userData.prDeadLift} lbs</div>
+          <div className="stat-desc">Personal Record</div>
         </div>
+      </div>
+      <div className="px-12 lg:px-56 md:px-56 text-center">
+        <p>{userData.bio}</p>
       </div>
     </div>
   )
